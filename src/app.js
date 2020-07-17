815fdfcb7896048fad80a880111aff096c7479b8
Marcos Samuel <m.samuel43.yahoo@gmail.com>
2020-07-17 08:43:42 -0300

    enable simple cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const buildPeerServer = require('./config/peerServer')
 
 const app = express()
 const router = require('./router')
 
+app.use(cors())
+
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(express.static('public'))
+
 app.set('views', 'views')
 app.set('view engine', 'hbs')
 
